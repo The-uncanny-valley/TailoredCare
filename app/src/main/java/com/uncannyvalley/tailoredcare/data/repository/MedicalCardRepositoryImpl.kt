e64a407763535cf456cbfec2eb9241c4893d4b79
The-uncanny-valley <runcannyvalley@gmail.com>
2026-02-04 20:03:18 +0300

    feat(data): implement MedicalCardRepository in data layer

---
+++ app/src/main/java/com/uncannyvalley/tailoredcare/data/repository/MedicalCardRepositoryImpl.kt
@@ -0,0 +1,37 @@
+package com.uncannyvalley.tailoredcare.data.repository
+
+import com.uncannyvalley.tailoredcare.data.local.dao.MedicalCardDao
+import com.uncannyvalley.tailoredcare.data.mappers.toDomain
+import com.uncannyvalley.tailoredcare.data.mappers.toEntity
+import com.uncannyvalley.tailoredcare.domain.model.MedicalCard
+import com.uncannyvalley.tailoredcare.domain.repository.MedicalCardRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class MedicalCardRepositoryImpl(
+    private val medicalCardDao: MedicalCardDao
+) : MedicalCardRepository {
+
+    override fun observeMedicalCards(): Flow<List<MedicalCard>> =
+        medicalCardDao.observeALl()
+            .map { list -> list.map { it.toDomain() } }
+
+    override fun observeMedicalCardByOwner(ownerId: Long): Flow<List<MedicalCard>> =
+        medicalCardDao.observerByOwner(ownerId)
+            .map { list -> list.map { it.toDomain() } }
+
+    override suspend fun getMedicalCardById(id: Long): MedicalCard? =
+        medicalCardDao.getById(id)?.toDomain()
+
+    override suspend fun addMedicalCard(card: MedicalCard) {
+        medicalCardDao.insert(card.toEntity())
+    }
+
+    override suspend fun updateMedicalCard(card: MedicalCard) {
+        medicalCardDao.update(card.toEntity())
+    }
+
+    override suspend fun deleteMedicalCard(card: MedicalCard) {
+        medicalCardDao.delete(card.toEntity())
+    }
+}
