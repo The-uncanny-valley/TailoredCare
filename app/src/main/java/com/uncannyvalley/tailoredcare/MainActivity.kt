b770a76c22b3a53956338676a0654461f42b25c4
The-uncanny-valley <runcannyvalley@gmail.com>
2026-02-06 00:15:55 +0300

    fix(hilt): annotate MainActivity with @AndroidEntryPoint to enable dependency injection

--- app/src/main/java/com/uncannyvalley/tailoredcare/MainActivity.kt
+++ app/src/main/java/com/uncannyvalley/tailoredcare/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.uncannyvalley.tailoredcare.presentation.navigation.AppNavHost
 import com.uncannyvalley.tailoredcare.presentation.theme.TailoredCareTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
