d2596bf55fca5e97724b83611ae5815192fdcf42
The-uncanny-valley <runcannyvalley@gmail.com>
2026-02-05 19:38:00 +0300

    feat(theme): integrate Poppins and Rubik fonts with corresponding TextStyles

--- app/src/main/java/com/uncannyvalley/tailoredcare/presentation/theme/Type.kt
+++ app/src/main/java/com/uncannyvalley/tailoredcare/presentation/theme/Type.kt
@@ -2,9 +2,20 @@ package com.uncannyvalley.tailoredcare.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.uncannyvalley.tailoredcare.R
+
+val Rubik = FontFamily(
+    Font(R.font.rubik_spray_paint_regular, FontWeight.Normal)
+)
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_semi_bold, FontWeight.SemiBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -13,8 +24,24 @@ val Typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
+        letterSpacing = 0.2.sp
+    ),
+
+    labelLarge = TextStyle(
+        fontFamily = Rubik,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.02.sp
+    ),
+
+    titleMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
